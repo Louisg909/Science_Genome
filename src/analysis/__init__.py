b917318583e6b66897e838e66da9bb18c7fa8a6d
Louis <93020904+Louisg909@users.noreply.github.com>
2026-05-11 10:00:08 +0100

    Add corpus inheritance pipeline and tests (#18)

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,5 +1,6 @@
 from .citation_dag import build_citation_dag, enforce_acyclicity_by_time
 from .inheritance import InheritanceResult, solve_inheritance
+from .inheritance_pipeline import PaperInheritanceFit, fit_inheritance_for_corpus
 from .structure import (
     embedding_norm_diagnostics,
     nearest_neighbor_density,
@@ -9,9 +10,11 @@ from .structure import (
 
 __all__ = [
     "InheritanceResult",
+    "PaperInheritanceFit",
     "build_citation_dag",
     "enforce_acyclicity_by_time",
     "embedding_norm_diagnostics",
+    "fit_inheritance_for_corpus",
     "nearest_neighbor_density",
     "nearest_neighbors",
     "similarity_matrix",
