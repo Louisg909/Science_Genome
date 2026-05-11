811ad967b600d3a70486b58645d173ffc81b174c
Louis <93020904+Louisg909@users.noreply.github.com>
2026-05-11 09:56:14 +0100

    Add citation DAG utilities and paper lineage metadata

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,3 +1,4 @@
+from .citation_dag import build_citation_dag, enforce_acyclicity_by_time
 from .inheritance import InheritanceResult, solve_inheritance
 from .structure import (
     embedding_norm_diagnostics,
@@ -8,6 +9,8 @@ from .structure import (
 
 __all__ = [
     "InheritanceResult",
+    "build_citation_dag",
+    "enforce_acyclicity_by_time",
     "embedding_norm_diagnostics",
     "nearest_neighbor_density",
     "nearest_neighbors",
